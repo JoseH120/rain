ea05727b9cbb7a001a000a44a3d4dfb60cd11b51
jose hernandez <reyes_7374@outlook.es>
2025-05-01 17:43:54 -0600

    inicio de repositorio

---
+++ src/com/personal/rain/Game.java
@@ -0,0 +1,141 @@
+package com.personal.rain;
+
+import com.personal.rain.graphics.Screen;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+
+    private static final long serialVersionUID = 1L;
+
+    // THE SIZE OF THE WINDOWS
+    // The width of the windows to be display
+    public static int width = 300;
+
+    // The height of the windows to be display
+//    public static int height = width / 16 * 9;
+    public static int height = 168;
+
+    // How much the graphic scale is going tobe 
+    public static int scale = 3;
+
+    // Title of the frame
+    private static final String title = "Rain";
+
+    // Process to do multiples purpose
+    private Thread thread;
+
+    // ventana
+    private JFrame frame;
+
+    private boolean running = false;
+
+    private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+    private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
+    private Screen screen;
+
+    public Game() {
+        Dimension size = new Dimension(width * scale, height * scale);
+        setPreferredSize(size);
+        screen = new Screen(width, height);
+        frame = new JFrame();
+    }
+
+    // synchronized preventing thread interference
+    public synchronized void start() {
+        running = true;
+        thread = new Thread(this, "Display");
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        running = false;
+        try {
+            thread.join();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        // very precise for nanoSecond
+        long lastTime = System.nanoTime();
+        long timer = System.currentTimeMillis();
+
+        // divide one billion with 60
+        final double ns = 1_000_000_000.0 / 60.0;
+        double delta = 0;
+        int frames = 0;
+        int updates = 0;
+        while (running) {
+            long now = System.nanoTime();
+            // divide de diference between 
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1) {
+                update();
+                updates++;
+                delta--;
+            }
+            render();
+            frames++;
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                frame.setTitle(title + "  |  " + updates + " ups, " + frames + " fps");
+                System.out.println(updates + " ups, " + frames + " fps");
+                updates = 0;
+                frames = 0;
+            }
+        }
+        stop();
+    }
+
+    public void update() {
+
+    }
+
+    public void render() {
+        BufferStrategy bs = getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(3);
+            return;
+        }
+        screen.clear();
+        screen.render();
+
+        for (int i = 0; i < pixels.length; i++) {
+            pixels[i] = screen.pixels[i];
+        }
+
+        Graphics g = bs.getDrawGraphics();
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, getWidth(), getHeight());
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+        g.dispose();
+        bs.show();
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.frame.setResizable(false);
+        game.frame.setTitle("Rain");
+        game.frame.add(game);
+        game.frame.pack();
+        game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        game.frame.setLocationRelativeTo(null);
+        game.frame.setVisible(true);
+
+        game.start();
+    }
+
+}
---
+++ src/com/personal/rain/graphics/Screen.java
@@ -0,0 +1,35 @@
+package com.personal.rain.graphics;
+
+public class Screen {
+
+    private int width;
+    private int height;
+    public int[] pixels;
+
+    public Screen(int width, int height) {
+        this.width = width;
+        this.height = height;
+        pixels = new int[this.width * this.height];  // 50,400
+    }
+
+    public void clear() {
+        for (int i = 0; i < pixels.length; i++) {
+            pixels[i] = 0;
+        }
+    }
+
+    public void render() {
+        for (int y = 0; y < height; y++) {
+            if (y < 0 && y >= height) {
+                break;
+            }
+            for (int x = 0; x < width; x++) {
+                if (x < 0 && x >= width) {
+                    break;
+                }
+                pixels[x + y * width] = 0xff00ff;
+            }
+        }
+    }
+
+}
